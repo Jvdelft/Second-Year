4c56277486940ce6aab8b58eec169defb33647b2
Julien <Julien@DESKTOP-TSFEQAS.pool.ulb.ac.be>
2019-04-29 10:48:31 +0200

    1

--- Projet d'info/src/view/ActionPanel.java
+++ Projet d'info/src/view/ActionPanel.java
@@ -35,7 +35,6 @@ public class ActionPanel extends JPanel implements ActionListener {
 	private ArrayList<JButton> visibleButtons = new ArrayList<JButton>();
 	private ArrayList<ActivableObject> activableObjects;
 	public ActionPanel() {
-		System.out.println("EY");
 		limits.weightx = 1;
 		limits.weighty = 1;
 		initButton(new JButton("EAT"));
