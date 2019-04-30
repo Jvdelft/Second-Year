aa724166f3cc3504f3c47e631e075c683f4cf3ac
charlox456 <charlywitmeur@hotmail.com>
2019-04-30 22:16:35 +0200

    test 10 000

--- Projet d'info/src/Main.java
+++ Projet d'info/src/Main.java
@@ -4,6 +4,7 @@ import view.Window;
 public class Main {
     public static void main(String[] args) {
         Window window = Window.getInstance();
+        System.out.println("test 10 000");
 
     }
 }
